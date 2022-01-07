270e3169d59fe8691d25107785752886f291a219
zeddo123 <m.drissi@protonmail.com>
2022-01-07 23:02:21 +0100

    Add test for xml.update_project_file

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,6 +1,6 @@
 import os
 
-from .fixtures import test_directory
+from .fixtures import test_directory, initialized
 from squirrel import xml
 from squirrel.vars import DIRECTORY_NAME
 from squirrel.squirrel import _main
@@ -30,3 +30,35 @@ def test_get_data_from_project_file(test_directory):
         'project-type': 'text'
     }
 
+
+def test_update_project_file(initialized):
+    xml.update_project_file({
+        'name': 'test_update_project_file',
+        'goal': 666,
+        'description': None,
+        'due': None,
+        'project-type': None,
+    })
+
+    assert xml.get_data_from_project_file() == {
+        'name': 'test_update_project_file',
+        'path': os.path.join(os.getcwd(), DIRECTORY_NAME),
+        'goal': '666',
+        'description': None,
+        'due-date': None,
+        'project-type': 'text',
+    }
+
+    xml.update_project_file({
+        'name': 'test_when_not_all_args_are_passed',
+        'goal': 666,
+    })
+
+    assert xml.get_data_from_project_file() == {
+        'name': 'test_when_not_all_args_are_passed',
+        'path': os.path.join(os.getcwd(), DIRECTORY_NAME),
+        'goal': '666',
+        'description': None,
+        'due-date': None,
+        'project-type': 'text',
+    }
