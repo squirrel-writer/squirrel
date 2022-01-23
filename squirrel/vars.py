7fa28b1c1c61e4a97136d9b27fcaf2df7d7c9df4
Henning Leth <leth.henning@gmail.com>
2022-01-23 16:11:30 +0100

    Add 'ignore' in  'vars.py' and updated 'watch.py'

--- squirrel/vars.py
+++ squirrel/vars.py
@@ -10,10 +10,12 @@ WATCH_FILENAME = 'watch-data.xml'
 DAEMON_NAME = 'squirreld'
 WATCH_DAEMON_PIDFILE = f'{DAEMON_NAME}.pid'
 DAEMON_LOGFILE = f'{DAEMON_NAME}.log'
+IGNORE_FILE = 'ignore'
 project_file_path = os.path.join(DIRECTORY_NAME, PROJECT_FILENAME)
 watch_file_path = os.path.join(DIRECTORY_NAME, WATCH_FILENAME)
 watch_daemon_pidfile_path = os.path.join(DIRECTORY_NAME, WATCH_DAEMON_PIDFILE)
 watch_daemon_logfile_path = os.path.join(DIRECTORY_NAME, DAEMON_LOGFILE)
+ignore_file_path = os.path.join(DIRECTORY_NAME, IGNORE_FILE)
 
 # TODO: find artist and credit him
 squirrel_art = """
