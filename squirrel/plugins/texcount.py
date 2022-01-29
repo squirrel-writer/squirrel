d4b9392ae0ba1bf1d6d58d0b211e4664c2c74b76
zeddo123 <m.drissi@protonmail.com>
2022-01-29 13:18:09 +0100

    Update texcount plugin to implement plugin arch

--- squirrel/plugins/texcount.py
+++ squirrel/plugins/texcount.py
@@ -6,19 +6,25 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def get_count(files) -> int:
-    # remove any non latex files (crucial when we have bib files)
-    files = list(filter(lambda x: os.path.splitext(x)[1] == '.tex', files))
+class Plugin:
+    name = 'texcount'
+    description = 'Count latex files with `texcount`'
+    requires = ()
 
-    output = subprocess.run(
-        f'texcount -0 -sum -q {" ".join(files)}',
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
+    def get_count(files) -> int:
+        # remove any non latex files (crucial when we have bib files)
+        files = list(filter(lambda x: os.path.splitext(x)[1] == '.tex', files))
 
-    total = list(filter(lambda x: x.isnumeric(), output.stdout.split('\n')))
-    if len(total) > 0:
-        return int(total[0])
-    logger.debug('Did not find value after parsing `texcount` command')
-    return 0
+        output = subprocess.run(
+            f'texcount -0 -sum -q {" ".join(files)}',
+            shell=True,
+            capture_output=True,
+            text=True,
+        )
+
+        total = list(filter(lambda x: x.isnumeric(),
+                     output.stdout.split('\n')))
+        if len(total) > 0:
+            return int(total[0])
+        logger.debug('Did not find value after parsing `texcount` command')
+        return 0
