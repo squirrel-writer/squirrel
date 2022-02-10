26f85a52e04295d299f5a2558dd2c4b6e0037538
zeddo123 <m.drissi@protonmail.com>
2022-02-10 01:46:04 +0100

    Add unittests for `data` command.
    In the pursuit of more tests ref #16

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from .fixtures import initialized, test_directory, watching
+from .fixtures import initialized, test_directory, watching, one_watch_added
 from squirrel.squirrel import _main
 from squirrel.vars import DIRECTORY_NAME, project_file_path, watch_file_path, watch_daemon_pidfile_path, watch_daemon_logfile_path
 from squirrel import xml
@@ -51,12 +51,6 @@ def test_correct_set_command(initialized):
     assert project_data['due-date'] == '01/01/2022'
 
 
-def test_incorrect_set_command(test_directory):
-    """Test the set command when the project is not initialized"""
-    return_code = _main(['set', '-p', 'texcount'])
-    assert return_code != 0
-
-
 def test_watch_command_after_init(watching):
     # test if log file was created
     assert os.path.isfile(watch_daemon_logfile_path)
@@ -64,21 +58,63 @@ def test_watch_command_after_init(watching):
     assert os.path.isfile(watch_daemon_pidfile_path)
 
 
-def test_watch_command_before_init(test_directory):
-    # If a watche was started without initializing the project
-    # it should return a non 0 return code
-    return_code = _main(['watch', 'start'])
-    assert return_code != 0
-
-
-def test_overview_before_init(test_directory):
-    return_code = _main(['overview'])
-    assert return_code != 0
-
-    return_code = _main(['overview', '--graph'])
-    assert return_code != 0
-
-
 def test_overview_after_init(initialized):
     return_code = _main(['overview'])
     assert return_code == 0
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ['set', '-p', 'texcount'],
+        ['watch', 'start'],
+        ['overview'],
+        ['overview', '--graph'],
+        ['data', '--today'],
+        ['data', '--all']
+    ]
+)
+def test_commands_before_init(args, test_directory, capsys):
+    """All commands (except init):
+    * return an non zero return code.
+    * spit out some error."""
+    return_code = _main(args)
+    out, err = capsys.readouterr()
+
+    assert return_code != 0
+    assert err != ''
+
+
+@pytest.mark.parametrize(
+    'args',
+    [
+        ['data', '--today'],
+        ['data', '--all']
+    ]
+
+)
+def test_data_command_after_init(args, initialized, capsys):
+    return_code = _main(args)
+    out, err = capsys.readouterr()
+
+    assert return_code == 0
+    assert err == ''
+    assert out == ''
+
+
+def test_data_today_after_watch(one_watch_added, capsys):
+    return_code = _main(['data', '--today'])
+    out, err = capsys.readouterr()
+
+    assert return_code == 0
+    assert err == ''
+    assert out == f'{one_watch_added[1]}, {one_watch_added[0]}\n'
+
+
+def test_data_all_after_watch(one_watch_added, capsys):
+    return_code = _main(['data', '--all'])
+    out, err = capsys.readouterr()
+
+    assert return_code == 0
+    assert err == ''
+    assert out == f"{one_watch_added[1].date().strftime('%d/%m/%Y')}, {one_watch_added[0]}\n"
